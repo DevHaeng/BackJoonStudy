c29f267c9fc44555a527fe8f4c1db0270aae0784
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2024-02-10 21:54:00 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 160 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,25 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int number = sc.nextInt();
-        int reverse = sc.nextInt();
-        int temp = 0;
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int number = Integer.parseInt(st.nextToken());
+        int reverse = Integer.parseInt(st.nextToken());
+        
         ArrayList<Integer> basket = new ArrayList<>();
         for (int i = 1; i <= number; i++) {
             basket.add(i);
         }
 
         for (int i = 0; i < reverse; i++) {
-            int a = sc.nextInt()-1;
-            int b = sc.nextInt()-1;
-
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            
             while (a < b) {
-                temp = basket.get(a);
+                int temp = basket.get(a);
                 basket.set(a, basket.get(b));
                 basket.set(b, temp);
                 a++;
@@ -30,5 +34,6 @@ public class Main {
         for (int i : basket) {
             System.out.print(i + " ");
         }
+        br.close();
     }
 }
