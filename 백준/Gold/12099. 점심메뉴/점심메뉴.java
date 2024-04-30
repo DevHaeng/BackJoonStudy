5594bfe2e3e7d7a6fdad7a5637a814400c19133f
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2024-04-30 22:50:10 +0900

    [Gold IV] Title: 점심메뉴, Time: 2296 ms, Memory: 43228 KB -BaekjoonHub

--- 백준/Gold/12099. 점심메뉴/점심메뉴.java
+++ 백준/Gold/12099. 점심메뉴/점심메뉴.java
@@ -8,17 +8,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 점심 메뉴의 수
-        int Q = Integer.parseInt(st.nextToken()); // 점심을 같이 먹는 기간
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
         int[] hot = new int[N];
         int[] sweet = new int[N];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             hot[i] = Integer.parseInt(st.nextToken());
             sweet[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] sameMenu = new int[Q];
+        int[] favoriteMenu = new int[Q];
 
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
@@ -29,14 +30,13 @@ public class Main {
 
             for (int j = 0; j < N; j++) {
                 if (hot[j] >= u && hot[j] <= v && sweet[j] >= x && sweet[j] <= y) {
-                    sameMenu[i]++;
+                    favoriteMenu[i]++;
                 }
             }
-            // 각 날의 매운맛 구간[u, v], 단맛 구간[x, y]
         }
+
         for (int i = 0; i < Q; i++) {
-            System.out.println(sameMenu[i]);
+            System.out.println(favoriteMenu[i]);
         }
-        // Q개의 줄에 줄마다 각 날의 영우와 승관이가 둘 다 좋아하는 메뉴의 수를 출력
     }
 }
