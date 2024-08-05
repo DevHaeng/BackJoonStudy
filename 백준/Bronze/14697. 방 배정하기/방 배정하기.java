e4f4caf8fd327c7115c01eef813e0331b762ca94
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2024-08-05 19:42:48 +0900

    [Bronze II] Title: 방 배정하기, Time: 104 ms, Memory: 14388 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.java
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,34 +12,17 @@ public class Main {
         int c = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        int[] num = {a, b, c};
-        Arrays.sort(num);
-
-        int result = 0;
-
-        if (n % num[0] == 0 || n % num[1] == 0 || n % num[2] == 0) {
-            System.out.println(1);
-            return;
+        for (int i = 0; i <= n / a; i++) {
+            for (int j = 0; j <= n / b; j++) {
+                for (int k = 0; k <= n / c; k++) {
+                    if (a * i + b * j + c * k == n) {
+                        System.out.println(1);
+                        return;
+                    }
+                }
+            }
         }
 
-        if (n % num[2] != 0) {
-            result = n % num[2];
-        } else {
-            System.out.println(1);
-            return;
-        }
-
-        if (result % num[1] != 0) {
-            result = result % num[1];
-        } else {
-            System.out.println(1);
-            return;
-        }
-
-        if (result % num[0] != 0) {
-            System.out.println(0);
-        } else {
-            System.out.println(1);
-        }
+        System.out.println(0);
     }
 }
