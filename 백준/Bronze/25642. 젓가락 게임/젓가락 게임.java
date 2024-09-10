e898c325aa8b360c252b45c59cd79ba5066f237e
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2024-09-10 21:36:05 +0900

    [Bronze III] Title: 젓가락 게임, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/25642. 젓가락 게임/젓가락 게임.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        while (true) {
+            b += a;
+            if (b >= 5) {
+                System.out.println("yt");
+                break;
+            }
+            a += b;
+            if (a >= 5) {
+                System.out.println("yj");
+                break;
+            }
+        }
+    }
+}
