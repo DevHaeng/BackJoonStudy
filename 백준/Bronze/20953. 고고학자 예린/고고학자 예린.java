4c041ab1b8b8a47fcb7eb4003f0e27380c582b6d
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2024-09-23 17:25:17 +0900

    [Bronze II] Title: 고고학자 예린, Time: 384 ms, Memory: 40324 KB -BaekjoonHub

---
+++ 백준/Bronze/20953. 고고학자 예린/고고학자 예린.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long result = (long) (a + b) * (a + b) * (a + b - 1) /2;
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
