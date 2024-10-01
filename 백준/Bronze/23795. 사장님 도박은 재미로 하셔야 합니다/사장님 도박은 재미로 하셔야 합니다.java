12c3fb0b60102cb250da759e575e8bcc941cd5f2
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2024-10-01 15:38:23 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == -1) break;
+            sum += n;
+        }
+        System.out.println(sum);
+    }
+}
