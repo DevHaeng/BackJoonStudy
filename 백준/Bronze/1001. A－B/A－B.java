6977e65039a2625f26fa0e177f6adb0759489ce1
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2024-12-02 17:30:29 +0900

    [Bronze V] Title: A-B, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        
+
         System.out.println(a - b);
     }
 }
