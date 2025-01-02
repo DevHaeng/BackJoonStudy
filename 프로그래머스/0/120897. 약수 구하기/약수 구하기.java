ce9c80c83052a740daec4788468bdbbe5518dff7
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2025-01-02 16:09:00 +0900

    [level 0] Title: 약수 구하기, Time: 3.01 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> divisors = new ArrayList<>();
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                divisors.add(i); 
+                if (i != n / i) {
+                    divisors.add(n / i); 
+                }
+            }
+        }
+
+        Collections.sort(divisors);
+
+        return divisors.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
