bf5871789ffe5c236e1b2c244a89c25cc82153de
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2025-02-08 18:35:22 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] common) {
+        int n = common.length;
+
+        if (common[1] - common[0] == common[2] - common[1]) {
+            int d = common[1] - common[0];
+            return common[n - 1] + d;
+        }
+
+        int r = common[1] / common[0];
+        return common[n - 1] * r;
+    }
+}
